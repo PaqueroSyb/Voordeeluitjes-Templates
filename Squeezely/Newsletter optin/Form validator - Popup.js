8f7fef434f6a0a4a9ed44824d9d9ddeef4911faa
PaqueroSyb <sybrenboss@gmail.com>
2022-01-05 16:47:56 +0100

    Styling changes

--- Squeezely/Newsletter optin/Form validator - Popup.js
+++ Squeezely/Newsletter optin/Form validator - Popup.js
@@ -14,18 +14,18 @@ if (currentPage.indexOf('vakantiepark') !== -1) {
 }
 
 inputEmail.addEventListener('input', function() {
-    let showErrorMessage = document.querySelector('.error-message');
-    emailValue = document.querySelector('.sqzly-emailfield').value;
-    let emailValid = pattern.test(emailValue);
-    if (emailValid === true) {
-        showErrorMessage.classList.add('hide');
-    } else {
-        showErrorMessage.classList.remove('hide');
-        showErrorMessage.innerText = 'e-mailadres is niet geldig';
-    }
-    if (emailValue === '') {
-        showErrorMessage.innerText = "dit veld is verplicht";
-    }
+  let showErrorMessage = document.querySelector('.error-message');
+  emailValue = document.querySelector('.sqzly-emailfield').value;
+  let emailValid = pattern.test(emailValue);
+  if (emailValid === true) {
+    showErrorMessage.classList.add('hide');
+  } else {
+    showErrorMessage.classList.remove('hide');
+    showErrorMessage.innerText = 'e-mailadres is niet geldig';
+  }
+  if (emailValue === '') {
+    showErrorMessage.innerText = "dit veld is verplicht";
+  }
 });
 
 submitButton.addEventListener('click', submitForm, false);
@@ -36,25 +36,25 @@ inputEmail.addEventListener('keydown', function(key) {
 });
 
 function submitForm(submitElement) {
-    let emailValid = pattern.test(emailValue);
-    if (emailValid === false) {
-        inputContainer.setAttribute('class', 'sqzly-fields input-invalid');
-    } else {
-        _sqzl.push({
-            "anonymize": "no",
-        });
-        // preferenceHotel = document.getElementById('preference-hotel').checked ? "Ja" : "Nee";
-        // preferenceVakantiepark = document.getElementById('preference-vakantiepark').checked ? "Ja" : "Nee";
-        _sqzl.push({
-            "event": "NewsletterSubscribeConfirm",
-            "email": emailValue,
-            "custom_email_preference_hotel": "Ja",
-            // "custom_email_preference_vakantiepark": preferenceVakantiepark,
-            "custom_email_batch": "Discount tool",
-        });
-        _sqzl.push({
-            "anonymize": "yes",
-        });
+  let emailValid = pattern.test(emailValue);
+  if (emailValid === false) {
+    inputContainer.setAttribute('class', 'sqzly-fields input-invalid');
+  } else {
+    _sqzl.push({
+      "anonymize": "no",
+    });
+    // preferenceHotel = document.getElementById('preference-hotel').checked ? "Ja" : "Nee";
+    // preferenceVakantiepark = document.getElementById('preference-vakantiepark').checked ? "Ja" : "Nee";
+    _sqzl.push({
+      "event": "NewsletterSubscribeConfirm",
+      "email": emailValue,
+      "custom_email_preference_hotel": "Ja",
+      // "custom_email_preference_vakantiepark": preferenceVakantiepark,
+      "custom_email_batch": "Discount tool",
+    });
+    _sqzl.push({
+      "anonymize": "yes",
+    });
     }
     if (submitElement == 'Enter key') {
       _document.querySelector('#sqzl_div-10-129 .szqly-modal-content').classList.add('sqzly-collapse');
